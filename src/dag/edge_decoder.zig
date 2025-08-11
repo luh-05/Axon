bf4a3e181ca49c2ae7346f8d8232d93f20074d6a
Luh-code <Lasse@hueffler.de>
2025-08-11 23:06:27 +0200

    vertex studd

---
+++ src/dag/edge_decoder.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const zon = std.zon;
+
+const vertex = @import("vertex.zig");
+
+pub fn createVertices(vertex: *const vertex.Vertex) void {}
--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+pub const vertex = @import("dag/vertex.zig");
 
 pub fn foo() void {
     std.debug.print("Hello Axon!\n", .{});
